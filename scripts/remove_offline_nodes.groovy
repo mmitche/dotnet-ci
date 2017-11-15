51b2563f1c46838b545458e6260ca0b22949167e
Matt Mitchell <mmitche@microsoft.com>
2017-11-15 09:34:09 -0800

    Remove disconnected nodes

--- scripts/remove_offline_nodes.groovy
+++ scripts/remove_offline_nodes.groovy
@@ -7,15 +7,17 @@ def nodes = Jenkins.instance.getNodes()
 nodes.each { node ->
   if (node instanceof AzureVMAgent) {
     if (node.getComputer() != null) {
-      if (node.getComputer().isTemporarilyOffline()) {
+      if (node.getComputer().isOffline()) {
         def cause = node.getComputer().getOfflineCause()
-        if (!(cause instanceof hudson.slaves.OfflineCause.UserCause)) {
-          println "Removing " + node.getComputer().getName() + " "
+        if (node.getComputer().isTemporarilyOffline() && !(cause instanceof hudson.slaves.OfflineCause.UserCause))
+          println "Removing " + node.getComputer().getName() + " " + (cause != null ? cause.getClass() : cause)
+          Jenkins.instance.removeNode(node)
+        } else if (node.getComputer().isOffline() && cause instanceof hudson.slaves.OfflineCause.ChannelTermination) {
+          println "Removing " + node.getComputer().getName() + " " + (cause != null ? cause.getClass() : cause)
           Jenkins.instance.removeNode(node)
         }
       }
     }
-  }
 }
 
 return true
